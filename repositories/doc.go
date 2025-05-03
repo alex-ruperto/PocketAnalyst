222f4278689d3030c002cd8f2090c3c1c13b8634
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2025-05-03 15:21:37 -0400

    started working on repositories

---
+++ repositories/doc.go
@@ -0,0 +1,9 @@
+/*
+Package repositories handle CRUD operations for specific database tables. They are responsible for:
+- Querying data
+- Inserting records
+- Deleting records
+- Updating records
+- Handling database-specific logic
+*/
+package repositories

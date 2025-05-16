ae80e603ff77496581c58e18b4923d2f64082be6
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2025-05-16 17:33:43 -0400

    started working on stock service

---
+++ services/stock_service.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"PocketAnalyst/clients"
+	"PocketAnalyst/errors"
+	"PocketAnalyst/models"
+	"PocketAnalyst/repositories"
+	"context"
+	"fmt"
+	"time"
+)
+
+// StockService handles business logic related to stock operations
+type StockService struct {
+	stockRepo   *repositories.StockRepository
+	alphaClient *clients.AlphaVantageClient
+}
+
+func NewStockService(
+	stockRepo *repositories.StockRepository,
+	alphaClient *clients.AlphaVantageClient,
+) *StockService {
+	return &StockService{
+		stockRepo:   stockRepo,
+		alphaClient: alphaClient,
+	}
+}
+
+func (s *StockService) FetchAndStoreStockData(ctx context.Context, symbol string) (int, error) {
+	// Fetch stock data from Alpha Vantage
+	stocks, err := s.alphaClient.FetchDaily(symbol)
+	if err != nil {
+		return 0, errors.NewServiceError("fetching stock data", err)
+	}
+
+	// If no data was returned, return early
+	if len(stocks) == 0 {
+		return 0, fmt.Errorf("no stock data found for symbol %s", symbol)
+	}
+
+	// Store the fetched data in the database
+	storedCount, err := s.stockRepo.StoreStockPrices(ctx, stocks)
+	if err != nil {
+		return 0, errors.NewServiceError("storing stock data", err)
+	}
+
+	return storedCount, nil
+}

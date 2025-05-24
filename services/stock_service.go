d99bd2db8c8e97ebb8efe7cd1470a7a9960fabef
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2025-05-24 12:55:47 -0400

    removed the checkifsymbolexists function since there wasn't really a need for it

--- services/stock_service.go
+++ services/stock_service.go
@@ -52,20 +52,44 @@ func (s *StockService) GetStockHistory(
 	symbol string,
 	startDate, endDate time.Time,
 ) ([]*models.Stock, error) {
-	// Validate date range
-	if startDate.After(endDate) {
-		return nil, errors.NewModelValidationError(
-			"StockService",
-			"date_range",
-			"start date cannot be after end  date",
-		)
+	// Validate date function parameters before anything
+	if err := s.validateInput(symbol, startDate, endDate); err != nil {
+		return nil, err
 	}
 
-	// Get stock prices from the repository
+	// Get stock prices from the database using the repository
 	stocks, err := s.stockRepo.RetrieveStocksFromDatabase(ctx, symbol, startDate, endDate)
 	if err != nil {
 		return nil, errors.NewServiceError("Retrieving stock history", err)
 	}
 
 	return stocks, nil
+}
+
+func (s *StockService) validateInput(symbol string, startDate, endDate time.Time) error {
+	if symbol == "" {
+		return errors.NewModelValidationError(
+			"StockService",
+			"symbol",
+			"symbol cannot be empty",
+		)
+	}
+
+	if startDate.After(endDate) {
+		return errors.NewModelValidationError(
+			"StockService",
+			"date_range",
+			"start date cannot be after end date",
+		)
+	}
+
+	if startDate.IsZero() || endDate.IsZero() {
+		return errors.NewModelValidationError(
+			"StockService",
+			"date_range",
+			"dates cannot be empty",
+		)
+	}
+
+	return nil
 }
